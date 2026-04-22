994cb46a96e11c751e756782f8d6d83f7e87abd1
HellDrac0 <161223015+HellDrac0@users.noreply.github.com>
2026-04-22 16:33:46 +0530

    Integrate RobustScaler into SVR model pipeline
    Refactor SVR model to use a pipeline with RobustScaler for scaling.

--- backend/svr_model.py
+++ backend/svr_model.py
@@ -4,6 +4,8 @@ import pandas as pd
 from sklearn.metrics import r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVR
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import RobustScaler
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_PATH = os.path.join(BASE_DIR, "..", "data", "cleaned_aqi_data.csv")
@@ -17,10 +19,6 @@ X = data[["PM2.5", "PM10", "NO", "NO2", "NOx", "NH3", "CO", "SO2", "O3", "Benzen
 # target
 y = data["AQI"]
 
-# fill missing values
-X = X.fillna(X.mean())
-y = y.fillna(y.mean())
-
 # split
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
@@ -29,12 +27,22 @@ X_train, X_test, y_train, y_test = train_test_split(
 # model
 model = SVR()
 
-# train
+#scaling
+
+model = Pipeline([
+    ("scaler", RobustScaler()),
+    ("regressor", SVR())
+])
+
+#train
+
+model = SVR()
 model.fit(X_train, y_train)
 
-# predict
+#predict
+
 prediction = model.predict(X_test)
 
 print("SVM Prediction")
 print(prediction)
-print("Accuracy:", r2_score(y_test, prediction))
+print("R2 Score:", r2_score(y_test, prediction))
